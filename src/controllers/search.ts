28d2fd431777139dbc0c257e7fe7bc1def42b655
arthur <arthurdesouzareis222@gmail.com>
2024-05-10 12:27:52 -0300

    feat: buscar pets

--- src/controllers/search.ts
+++ src/controllers/search.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const search = (req: Request, res: Response) => {
-  res.send('search');
+  let query = req.query.q as string;
+
+  let list = Pet.getFromName(query);
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query
+  });
 };
